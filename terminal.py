f15c6d4c68c5c93128ef2af4c98e4163215027a9
julio-cascalles <julio.cascalles@outlook.com>
2022-12-09 22:03:37 -0300

    [correção] Usando `exists` tanto para arquivo quanto para pasta

--- terminal.py
+++ terminal.py
@@ -12,8 +12,5 @@ class Terminal:
     def chdir(self, path: str):
         os.chdir(path)
 
-    def isfile(self, filename: str) -> bool:
-        return os.path.isfile(filename)
-
-    def exists(self, path: str) -> bool:
-        return os.path.exists(path)
+    def exists(self, filename: str) -> bool:
+        return os.path.exists(filename)
